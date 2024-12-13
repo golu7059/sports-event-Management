281e56e78bec2cd7477293a3541c8d895ef29113
golu Kumar <golukumarjaisidih@gmail.com>
2024-12-13 23:57:50 +0530

    used handError middleware
    used standradized error handeling

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,22 +1,31 @@
 import express from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+
 const app = express();
+
+// error handler:  
+import errorHandler from './middlewares/error.middleware.js';
 
 // importing routes
 import healthCheckRouter from './routes/healthCheck.route.js'
+import userRouter from './routes/user.route.js'
 
 app.use(cors({
         origin : process.env.CORS_URL,
-        Credentials : true
+        credentials : true
     }))
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true , limit : "16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())
 
 // routing
-console.log("before call")
 app.use('/api/v1/healthcheck',healthCheckRouter);
+app.use('/api/v1/users',userRouter)
+app.use(errorHandler)
 
 export {
     app
