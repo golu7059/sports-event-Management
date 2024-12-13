59c4417dcbd97c40e9b60e6267272e7a8df3eb74
golu Kumar <golukumarjaisidih@gmail.com>
2024-12-13 23:57:13 +0530

    solved prolem of multer to create temp file
    using path.resolve() multer can create automatically new temp file in
    the public folder

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -1,14 +1,19 @@
-import multer from 'multer'
+import multer from 'multer';
+import path from 'path';
 
+// Define the storage configuration
 const storage = multer.diskStorage({
-    destination :function(req,file,cb) {
-        cb(null,'./public/temp')
+    destination: function(req, file, cb) {
+        // Use path.resolve to create an absolute path
+        const tempDir = path.resolve('public', 'temp');
+        cb(null, tempDir);
     },
-    filename: function(req,file,cb){
-        cb(null,file.originalname)
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
     }
-})
+});
 
+// Export the multer upload middleware
 export const upload = multer({
     storage
-})
+});
