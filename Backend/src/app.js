c513c413029c71dbf10b9f59b30dd3d560981b10
golu Kumar <golukumarjaisidih@gmail.com>
2024-12-17 16:00:57 +0530

    added more routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,6 +11,9 @@ import errorHandler from './middlewares/error.middleware.js';
 // importing routes
 import healthCheckRouter from './routes/healthCheck.route.js'
 import userRouter from './routes/user.route.js'
+import teamRouter from './routes/team.route.js'
+import locationRouter from './routes/location.route.js'
+import matchRouter from './routes/match.route.js'
 
 app.use(cors({
         origin : process.env.CORS_URL,
@@ -25,6 +28,9 @@ app.use(cookieParser())
 // routing
 app.use('/api/v1/healthcheck',healthCheckRouter);
 app.use('/api/v1/users',userRouter)
+app.use('/api/v1/matches',matchRouter)
+app.use('/api/v1/teams',teamRouter)
+app.use('/api/v1/locations',locationRouter)
 app.use(errorHandler)
 
 export {
