967c6e61035c96473488a5caf6a305dee7e3ae58
golu Kumar <golukumarjaisidih@gmail.com>
2024-12-17 15:57:24 +0530

    updated team to teams

--- Backend/src/models/match.model.js
+++ Backend/src/models/match.model.js
@@ -1,44 +1,50 @@
-import mongoose,{Schema} from 'mongoose'
-import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const matchSchema = new Schema({
-    location : {
-        type : Schema.Types.ObjectId,
-        ref : "Location",
-        required : true
+const matchSchema = new Schema(
+  {
+    location: {
+      type: Schema.Types.ObjectId,
+      ref: "Location",
+      required: true,
     },
-    category : {
-        type : String,
-        required : true,
-        default : "Cricket"
+    category: {
+      type: String,
+      required: true,
+      default: "Cricket",
     },
-    numberOfPlayers : {
-        type : Number,
-        required : true,
-        default : 11
+    numberOfPlayers: {
+      type: Number,
+      required: true,
+      default: 11,
     },
-    team : {
-        type : Schema.Types.ObjectId,
-        ref : "Team",
-    },
+    teams: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Team",
+      },
+    ],
     time: {
-        type : Date
+      type: Date,
     },
-    isPaid : {
-        type : Boolean,
-        required : true, 
-        default : false
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
     },
-    organiser : {
-        type : Schema.Types.ObjectId,
-        ref : "User"
+    organiser: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required : true
     },
-    paymentOptions : {
-        type : Array
-    }
-},{
-    timestamps : true
-})
+    paymentOptions: {
+      type: Array,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-matchSchema.plugin(mongooseAggregatePaginate)
-export const Match = mongoose.model("Match",matchSchema);
+matchSchema.plugin(mongooseAggregatePaginate);
+export const Match = mongoose.model("Match", matchSchema);
