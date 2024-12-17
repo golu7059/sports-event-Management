495fc95300390712f5bfd85da182b465d7cb8ebf
golu Kumar <golukumarjaisidih@gmail.com>
2024-12-17 10:12:37 +0530

    formated properly

--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,23 +1,48 @@
-import { registerUser , loginUser, logout } from "../controllers/user.controller.js";
+import {
+  registerUser,
+  loginUser,
+  logout,
+  refreshAccessToken,
+  changeCurrentUserPassword,
+  getCurrentUser,
+  updateCurrentUserDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getMatchHistory,
+} from "../controllers/user.controller.js";
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const router = Router()
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name : "avatar",
-            maxCount : 1
-        },
-        {
-            name : "coverImage",
-            maxCount : 1
-        }
-    ]),
-    registerUser);
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 router.route("/login").get(loginUser);
-router.route("/logout").post(verifyJWT,logout)
+router.route("/refresh-token").post(refreshAccessToken);
 
-export default router
+// sercured routes
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-user").put(verifyJWT, updateCurrentUserDetails);
+router
+  .route("/udpate-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/update-coverImage")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/matchHistory").get(verifyJWT, getMatchHistory);
+router.route("/change-password").post(verifyJWT, changeCurrentUserPassword);
+router.route("/logout").post(verifyJWT, logout);
+
+export default router;
